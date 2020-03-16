6b97c5fd1f5dcd0f22ff6107722e534c04a20bd8
Mark McAdam <mark.a.mcadam@gmail.com>
2020-03-16 11:29:51 -0700

    chinook_queries updated

---
+++ chinook_queries.py
@@ -0,0 +1,38 @@
+import os 
+import sqlite3
+
+#DB_FILEPATH = "chinook.db"
+DB_FILEPATH = os.path.join(os.path.dirname(__file__), "./data", "chinook.db")
+
+connection = sqlite3.connect(DB_FILEPATH)
+connection.row_factory = sqlite3.Row
+print("Connection: ", connection)
+
+cursor = connection.cursor()
+print("Cursor: ", cursor)
+
+# query = "SELECT * FROM customers;"
+query = """
+SELECT
+    Country
+    ,count(distinct CustomerId) as CustomerCount -- > 59
+FROM customers
+GROUP BY Country
+ORDER BY CustomerCount DESC
+LIMIT 5
+"""
+
+
+
+# result = cursor.execute(query)
+# print("Result: ", result) # This returns a cursor object w/o results
+
+result2 = cursor.execute(query).fetchall()
+print("Result2: ", result2)
+
+for row in result2:
+    print(type(row))
+    print(row)
+    print(row["Country"])
+    print(row["CustomerCount"])
+    print("-------")
